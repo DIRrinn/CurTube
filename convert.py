8e7bc9dc3d1ead232de46d54ac7106b725054744
DIRrinn <cosmologicalhorizon@yandex.ru>
2026-01-06 19:40:14 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -2,8 +2,15 @@ import ffmpeg
 import os
 
 def convert_to_mp3(file, res, output):
-    stream = ffmpeg.input(output + "/" + file + res)
-    stream = ffmpeg.output(stream, output + "/" + file + ".mp3", loglevel="quiet")
-    ffmpeg.run(stream)
-    os.remove(output + "/" + file + res) 
+    source = output + "/" + file + res
+    new = output + "/" + ("CONVERTED") + file + ".mp3"
 
+    ffmpegGlobalArguments = ['-n']
+
+    stream = ffmpeg.input(source)
+    stream = ffmpeg.output(stream, new, loglevel="quiet")
+    stream = stream.global_args(*ffmpegGlobalArguments)
+    ffmpeg.run(stream)
+
+    os.remove(source)
+    os.rename(new, source)
