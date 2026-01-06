cfecb03dbf7db019d4a77639679a8efb5ab90ab8
DIRrinn <cosmologicalhorizon@yandex.ru>
2026-01-06 19:40:44 +0700

    Update download.py

--- download.py
+++ download.py
@@ -1,8 +1,8 @@
 # download.py
 from pytubefix import YouTube
 import json
-import convert
 import os
+import convert
 
 def get_configs():
     config_path = os.getcwd() + "/config.json"
@@ -12,15 +12,15 @@ def get_configs():
     url = variables["url"]
     res = variables["resolution"]
     symbols_remove = variables["incorrect_symbols_remove"]
-    convert = variables["convert_to_mp3"]
+    ifconvert = variables["convert_to_mp3"]
     output = variables["output_path"]
 
-    return url, res, symbols_remove, convert, output
+    return url, res, symbols_remove, ifconvert, output
 
 
-def download(url, res, symbols_remove, convert, output):
+def download(url, res, symbols_remove, ifconvert, output):
 
-    yt = YouTube(url, use_oauth=False, allow_oauth_cache=True)
+    yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
 
     #incorrect symbols might break the save process, so they need to be deleted from file's name
     if symbols_remove == True:
@@ -40,7 +40,7 @@ def download(url, res, symbols_remove, convert, output):
     else:
         file = yt.title
 
-    if res == ".m4a":
+    if res == ".m4a" or res == ".mp3":
         ys = yt.streams.get_audio_only()
     elif res == ".mp4":
         #360p support only
@@ -48,7 +48,15 @@ def download(url, res, symbols_remove, convert, output):
     #elif user_choice == ".mp4_720":
         #dosomething
 
+    if os.path.isfile(output + "/" + file + res):
+        os.remove(output + "/" + file + res)
+
     ys.download(output_path=output, filename=file + res)
+
+    if res == ".mp3":
+        # Если mp3 по дефолту, то конвертируем скрытно
+        convert.convert_to_mp3(file, res, output)
+
     return file
 
 def get_title():
